3b6ac3a1dcc4ba8188ba96eac3f13ddc3742ac37
marzia.magro <mrz.magro@gmail.com>
2025-05-22 17:35:07 +0200

    Service update

--- src/main/java/com/example/hotel/Service/ReservationService.java
+++ src/main/java/com/example/hotel/Service/ReservationService.java
@@ -1,8 +1,13 @@
 package com.example.hotel.Service;
 
+import com.example.hotel.Entity.ReservationEntity;
+import com.example.hotel.Mapper.ReservationMapping;
 import com.example.hotel.Repository.ReservationRepository;
+import com.example.reservation.model.ReservationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReservationService {
@@ -13,23 +18,23 @@ public class ReservationService {
     public ReservationService(ReservationRepository reservationRepository) {
         this.reservationRepository = reservationRepository;
     }
-//
-//    // TODO:  GET /reservations
-//    public List<ReservationDto> getAllReservations() {
-//        return List.of();
-//    }
-//
-//    // POST /reservations
-//    public ReservationDto createReservation(ReservationDto reservationDto) {
-//        Reservation reservation = ReservationMapping.convToEntity(reservationDto);
-//        reservation = reservationRepository.save(reservation);
-//        return ReservationMapping.convToDto(reservation);
-//    }
-//
-//    // TODO: DELETE /reservations/{id}
-//    public void deleteReservation(Integer id) {
-//
-//    }
+
+    // TODO:  GET /reservations
+    public List<ReservationDto> getAllReservations() {
+        return ReservationMapping.listDto(reservationRepository.findAll());
+    }
+
+    // POST /reservations
+    public ReservationDto createReservation(ReservationDto reservationDto) {
+        ReservationEntity reservation = ReservationMapping.convToEntity(reservationDto);
+        reservation = reservationRepository.save(reservation);
+        return ReservationMapping.convToDto(reservation);
+    }
+
+    //    // TODO: DELETE /reservations/{id}
+    public void deleteReservation(Integer reservationId) {
+        reservationRepository.deleteById(reservationId);
+    }
 //
 //    //TODO GET BY ID
 //    public ReservationDto getReservationById(Integer id) {
